bd800a03eb0701508dff308abdb0515030a44c3e
luterien <uguryoruk26@gmail.com>
2013-06-11 11:38:58 +0300

    migrate to dj1.5, part 1

--- projectbonus/urls.py
+++ projectbonus/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 from django.contrib.auth.views import password_change, password_change_done
 
 from apps.projects.views import *
@@ -29,8 +29,8 @@ urlpatterns = patterns('',
 
     url(r'^project/', include('apps.projects.urls')),
 
-    url(r'^about/', direct_to_template, {'template':'about.html'}),
-    url(r'^contact/', direct_to_template, {'template':'contact.html'}),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
+    url(r'^contact/', TemplateView.as_view(template_name="contact.html")),
 
     # profile urls
     url(r'^$',                                           login_user,         name="login"),
@@ -51,7 +51,6 @@ urlpatterns = patterns('',
 
     url(r'^organization/create/$',             CreateOrganization.as_view() , name="create_organization"),
     url(r'^organization/(?P<slug>[-\w]+)/$',             OrganizationDetails.as_view() , name="organization_details"),
-    url(r'^organization/(?P<slug>[-\w]+)/update/$', OrganizationUpdate.as_view(), name="update_organization"),
     url(r'^organization/(?P<organization_id>[-\d]+)/invite/', InviteToOrganization.as_view() , name="invite_to_organization"),
     url(r'^project/(?P<project_id>[-\d]+)/invite/$', InviteToProject.as_view(), name="invite_to_project"),
 
