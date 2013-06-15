98530fbecb275432691401d39379f02d8fe87246
luterien <uguryoruk24@gmail.com>
2013-06-15 21:20:41 +0300

    ajax fix

--- apps/profiles/ajax.py
+++ apps/profiles/ajax.py
@@ -1,16 +1,16 @@
-
-from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.utils import simplejson
 from django.shortcuts import render, get_object_or_404
+
 from apps.projects.models import Task
+from apps.profiles.models import Profile
 
 
 def assign_user(request):
     user_id = request.GET.get('user_id')
     task_id = request.GET.get('task_id')
 
-    u = User.objects.get(id=int(user_id))
+    u = Profile.objects.get(id=int(user_id))
     t = Task.objects.get(id=int(task_id))
 
     if u in t.project.people.all() and u not in t.people.all():
@@ -25,7 +25,7 @@ def remove_user(request):
     user_id = request.GET.get('user_id')
     task_id = request.GET.get('task_id')
 
-    p = User.objects.get(id=int(user_id))
+    p = Profile.objects.get(id=int(user_id))
     t = Task.objects.get(id=int(task_id))
 
     if p in t.project.people.all() and p in t.people.all():
