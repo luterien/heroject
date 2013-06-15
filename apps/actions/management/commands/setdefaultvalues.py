fadcc0aa96864d59d2538bec8d4790b668d1110d
luterien <uguryoruk24@gmail.com>
2013-06-15 22:52:27 +0300

    extra default values

--- apps/actions/management/commands/setdefaultvalues.py
+++ apps/actions/management/commands/setdefaultvalues.py
@@ -10,18 +10,19 @@ class Command(BaseCommand):
         
         # create the basic ActionTypes
         action_type_lst = [
-            ('create', 'created', ''),
-            ('delete', 'deleted', ''),
-            ('comment', 'commented', 'on'),
-            ('invite', 'invited', 'to'),
-            ('assign', 'assigned', 'to')
+            ('create',  'created',   '',   '%(user)s has created %(action_object)s'),
+            ('delete',  'deleted',   '',   '%(user)s has deleted %(action_object)s'),
+            ('comment', 'commented', 'on', '%(user)s has commented on %(target_object)s'),
+            ('invite',  'invited',   'to', '%(user)s has invited %(action_object)s to %(target_object)s'),
+            ('assign',  'assigned',  'to', '%(user)s has assigned %(action_object)s to %(target_object)s')
         ]
 
         for tpl in action_type_lst:
-            name, verb, prep = tpl
+            name, verb, prep, format = tpl
             at, ex = ActionType.objects.get_or_create(name=name,
                                                       verb=verb,
-                                                      preposition=prep)
+                                                      preposition=prep,
+                                                      format=format)
             at.save()
 
             print "ActionType created : %s" % ex
