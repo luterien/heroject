7881867c65174113e12db45d9d0495b82f8f8509
ercan <ercan@ercan-VirtualBox.(none)>
2013-06-20 10:38:19 +0300

    ugur cok pis cakistik gitti hersey aq

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.edit import UpdateView, CreateView
 from django.core.urlresolvers import reverse
 from apps.actions.forms import *
 from apps.projects.forms import *
