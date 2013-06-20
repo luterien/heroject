967703d08562d9487714b6dcedf2dea0787473a7
luterien <uguryoruk26@gmail.com>
2013-06-20 10:13:56 +0300

    access permission decorator

--- apps/profiles/decorators.py
+++ apps/profiles/decorators.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from functools import wraps
 
+from apps.projects.models import Project
 
 def anonymous_required(redirect_url=None):
     def decorator(function):
@@ -10,4 +11,20 @@ def anonymous_required(redirect_url=None):
             else:
                 return function(request, *args, **kwargs)
         return wraps(function)(_control)
+    return decorator
+
+
+
+def has_access_permission(cls=Project):
+    def decorator(function):
+        def _wrapped_view(request, *args, **kwargs):
+            
+            item = cls._default_manager.get(**kwargs)
+
+            if request.user in item.people.all():
+                return function(request, *args, **kwargs)
+
+            return function(request, *args, **kwargs)
+
+        return wraps(function)(_wrapped_view)
     return decorator
